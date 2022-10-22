a350f63515b7e0d01b8aabb01426b36231dd7ed3
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2022-10-23 01:27:27 +0530

    minimum cost to make string valid

---
+++ Stack/minimum_cost_to_make_string_valid.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+int findMinimumCost(string str) {
+  // Write your code here
+    if(str.length()&1)
+        return -1;
+    stack<char> s;
+    for(int i=0;i<str.length();i++)
+    {
+        char ch=str[i];
+        if(ch=='{')
+        {
+            s.push(ch);
+        }
+        else
+        {
+            if(!s.empty() && s.top()=='{')
+            {
+                s.pop();
+            }
+            else
+            {
+                s.push(ch);
+            }
+        }
+    }
+    int a=0,b=0;
+    while(!s.empty())
+    {
+        if(s.top()=='{')
+        {
+            a++;
+        }
+        else
+        {
+            b++;
+        }
+        s.pop();
+    }
+    return (a+1)/2 + (b+1)/2;
+}
