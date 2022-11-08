e7dc11fe5ef83059e78108c8f08c485dd8884a8c
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2022-11-09 02:40:23 +0530

    circular tour

---
+++ Queue/circular_tour.cpp
@@ -0,0 +1,25 @@
+int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       int balance=0;
+       int start=0;
+       int deficit=0;
+       for(int i=0;i<n;i++)
+       {
+           balance+=p[i].petrol-p[i].distance;
+           if(balance<0)
+           {
+               start=i+1;
+               deficit+=balance;
+               balance=0;
+           }
+       }
+       if(deficit+balance>=0)
+       {
+           return start;
+       }
+       else
+       {
+           return -1;
+       }
+    }
