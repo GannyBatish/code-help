20af444f163252049a44c6d0ef58dfd724e2b255
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2022-11-10 02:24:31 +0530

    Interleave The First Half Of The Queue With The Second Half using stack

--- Queue/interleaved_first_half_with_second_half_of_queue.cpp
+++ Queue/interleaved_first_half_with_second_half_of_queue.cpp
@@ -1,3 +1,4 @@
+//using another queue
 #include <bits/stdc++.h> 
 void interLeaveQueue(queue < int > & q) {
     // Write your code here.
@@ -14,6 +15,54 @@ void interLeaveQueue(queue < int > & q) {
     while(!q1.empty()){
         int val=q1.front();
         q1.pop();
+        q.push(val);
+        val=q.front();
+        q.pop();
+        q.push(val);
+    }
+}
+
+
+// using stack
+#include <bits/stdc++.h> 
+void interLeaveQueue(queue < int > & q) {
+    // Write your code here.
+    stack<int> s;
+    int n=q.size();
+    int i=0;
+    while(i<n/2)
+    {
+        int val=q.front();
+        q.pop();
+        s.push(val);
+        i++;
+    }
+    while(!s.empty())
+    {
+        int val=s.top();
+        s.pop();
+        q.push(val);
+    }
+    i=0;
+    while(i<n/2)
+    {
+        int val=q.front();
+        q.pop();
+        q.push(val);
+        i++;
+    }
+    i=0;
+    while(i<n/2)
+    {
+        int val=q.front();
+        q.pop();
+        s.push(val);
+        i++;
+    }
+    while(!s.empty())
+    {
+        int val=s.top();
+        s.pop();
         q.push(val);
         val=q.front();
         q.pop();
