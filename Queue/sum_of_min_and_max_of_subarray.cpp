8422334dd657afe5fe9a6ec9b16a2b731c9a96a2
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2022-11-11 03:17:45 +0530

    Sum of minimum and maximum elements of all subarrays of size k using deque

--- Queue/sum_of_min_and_max_of_subarray.cpp
+++ Queue/sum_of_min_and_max_of_subarray.cpp
@@ -1,3 +1,4 @@
+//brute force
 #include <bits/stdc++.h> 
 long long sumOfMaxAndMin(vector<int> &arr, int n, int k) {
 	// Write your code here.
@@ -14,4 +15,51 @@ long long sumOfMaxAndMin(vector<int> &arr, int n, int k) {
         ans+=maxi+mini;
     }
     return ans;
+}
+
+//using deque
+#include <bits/stdc++.h> 
+long long sumOfMaxAndMin(vector<int> &arr, int n, int k) {
+	// Write your code here.
+    deque<int> maxi,mini;
+    for(int i=0;i<k;i++)
+    {
+        while(!maxi.empty() && arr[maxi.back()]<=arr[i])
+        {
+            maxi.pop_back();
+        }
+        while(!mini.empty() && arr[mini.back()]>=arr[i])
+        {
+            mini.pop_back();
+        }
+        maxi.push_back(i);
+        mini.push_back(i);
+    }
+    long long sum=arr[maxi.front()]+arr[mini.front()];
+    for(int i=k;i<n;i++)
+    {
+        //removal
+        while(!maxi.empty() && i-maxi.front()>=k)
+        {
+            maxi.pop_front();
+        }
+        while(!mini.empty() && i-mini.front()>=k)
+        {
+            mini.pop_front();
+        }
+        
+        //add
+        while(!maxi.empty() && arr[maxi.back()]<=arr[i])
+        {
+            maxi.pop_back();
+        }
+        while(!mini.empty() && arr[mini.back()]>=arr[i])
+        {
+            mini.pop_back();
+        }
+        maxi.push_back(i);
+        mini.push_back(i);
+        sum+=arr[maxi.front()]+arr[mini.front()];
+    }
+    return sum;
 }
