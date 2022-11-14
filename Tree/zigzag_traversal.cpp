4f7f54c105795291ab84b17560a5a5225ea77163
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2022-11-15 01:25:47 +0530

    Zig-Zag travesal of binary tree Love Babbar Approach

--- Tree/zigzag_traversal.cpp
+++ Tree/zigzag_traversal.cpp
@@ -58,3 +58,41 @@ vector <int> zigZagTraversal(Node* root)
     	return ans;
     }
 
+
+
+    //love babbar Arrpoach
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	// Code here
+    	queue<Node*> q;
+    	vector<int> ans;
+    	q.push(root);
+    	bool leftToRight=true;
+    	while(!q.empty())
+    	{
+    	    int size=q.size();
+    	    vector<int> tempArr(size);
+    	    for(int i=0;i<size;i++)
+    	    {
+    	        int index=leftToRight?i:size-i-1;
+    	        Node* temp=q.front();
+    	        q.pop();
+    	        tempArr[index]=temp->data;
+    	        if(temp->left)
+    	        {
+    	            q.push(temp->left);
+    	        }
+    	        if(temp->right)
+    	        {
+    	            q.push(temp->right);
+    	        }
+    	    }
+    	    for(auto i:tempArr)
+    	    {
+    	        ans.push_back(i);
+    	    }
+    	    leftToRight=!leftToRight;
+    	}
+    	return ans;
+    }
+    
