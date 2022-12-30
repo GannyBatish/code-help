85691820c7a8fcc3c0e368aafe0a0e4d2e690664
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2022-12-31 02:40:01 +0530

    search in BST in constant space

--- BST/search_in_BST.cpp
+++ BST/search_in_BST.cpp
@@ -1,3 +1,5 @@
+//T(N)- O(Log N)
+//S(N)- O(Log N)
 bool searchInBST(BinaryTreeNode<int> *root, int x) {
     // Write your code here.
     if(root==NULL)
@@ -13,4 +15,30 @@ bool searchInBST(BinaryTreeNode<int> *root, int x) {
         return searchInBST(root->left,x);
     }
     return searchInBST(root->right,x);
+}
+
+
+
+
+//T(N)-O(log N)
+//S(N)-O(1)
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+    BinaryTreeNode<int> *temp=root;
+    while(temp!=NULL)
+    {
+        if(temp->data==x)
+        {
+            return true;
+        }
+        if(temp->data>x)
+        {
+            temp=temp->left;
+        }
+        else
+        {
+            temp=temp->right;
+        }
+    }
+    return false;
 }
