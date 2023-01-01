cb8f71b2d9cd93e053b8695fcc70fb158a5b76e3
Ganny Batish <ganny0223.be20@chitkara.edu.in>
2023-01-02 04:10:49 +0530

    Lowest Common Anscestor

---
+++ BST/lowest_common_ancestor.cpp
@@ -0,0 +1,22 @@
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+	// Write your code here
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    while(root!=NULL)
+    {
+        if(root->data>P->data && root->data>Q->data)
+        {
+            root=root->left;
+        }
+        else if(root->data<P->data && root->data<Q->data)
+        {
+            root=root->right;
+        }
+        else{
+            return root;
+        }
+    }
+}
